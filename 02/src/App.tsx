287931942247e511775b6bc79e8ff20b6d04e51d
MukeshAbhi <abhimukesh284@gmail.com>
2025-04-08 15:08:55 +0530

    nac bar

---
+++ 02/src/App.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from "./components/Navbar"
+
+function App() {
+ 
+
+  return (
+    <main className="mx-auto max-w-7xl overflow-x-hidden antialiased">
+      <div className="bg-image fixed inset-0 bg-cover bg-fixed bg-center">
+        <div className="relative z-10"> <Navbar /></div>
+      </div>
+      text
+    </main>
+  )
+}
+
+export default App
